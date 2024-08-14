2fef3010aef01c7c9c8741aba21282b9214d4762
Kashish Kesharwani <kashishkesharwani24@gamil.com>
2024-08-15 02:52:50 +0530

    Done Project work

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './Components2/Navbar';
+import Weather2 from './Components2/Weather2';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar></Navbar>
+    <Weather2></Weather2>
+          </div>
+  );
+}
+
+export default App;
---
+++ src/Components2/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Navbar(){
+    return (
+<div className=" flex  shadow-xl justify-between items-center px-8">
+
+    <h1 className="text-[25px] font-black cursor-pointer text-white">weather <span className="text-red-800"> Tracker</span> </h1>
+    </div>
+
+    )
+    }
+    export default Navbar;
